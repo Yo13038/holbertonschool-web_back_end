c241fc1a17a9178eabf48508acecd53aaae4522f
Yo13038 <13038@holbertonstudents.com>
2026-06-15 15:39:27 +0200

    asyncronous coroutine that take a int and wait a random delay

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Asynchronous coroutine that take a int argument
+and wait a random delay
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """asynchronous coroutine who wait for a delay"""
+
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
