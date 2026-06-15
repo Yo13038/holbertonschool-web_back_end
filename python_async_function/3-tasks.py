97c0483b88fe547a70274f6c825111db091e6e67
Yo13038 <13038@holbertonstudents.com>
2026-06-15 21:17:00 +0200

    function who take a int, return a asyncio

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module of a regular function and return a asyncio task
+"""
+import asyncio
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """function who take a int, return a asyncio"""
+    return asyncio.create_task(wait_random(max_delay))
